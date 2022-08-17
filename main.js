b24755c7d52cdc02d27ce159393fbd9aa98cc669
ifeawoyemi@gmail.com <ifeawoyemi@gmail.com>
2022-08-17 12:45:41 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const toggleBtn = () => {
+  const nav = document.getElementById('.navBar');
+  const toggleBtn = 
+
+document.querySelector('.hamburger_menu').classList.toggle('change');
+
+
+  if (window.getComputedStyle(navBar).display === "none") {
+    navBar.style.display = "block";
+  } else {
+     
+  }
+};
+
+function closeIcon(){
+  document.getElementById('navBar').style.width = '0%';
+}
